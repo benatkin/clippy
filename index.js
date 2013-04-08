a78b6b2e33b36678d4ab50ee59dfa60e96519e87
Ben Atkin <ben@benatkin.com>
2013-04-08 23:51:40 +0000

    add code for reading from stdin

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ module.exports = {
     if (this.cli.args.length > 0) {
       fs.readFileSync(this.cli.args[0], this.on_read_file.bind(this));
     } else {
-      // read from standard input
+      // TODO: move into function
+      process.stdin.resume();
+      process.stdin.setEncoding('utf8');
+      
+      process.stdin.on('data', function(chunk) {
+        process.stdout.write('data: ' + chunk);
+      });
+      
+      process.stdin.on('end', function() {
+        // TODO: call run function
+      });
     }
   },
   "on_read_file": function(err, data, action) {
